47c04622ca97d0602c686b7789dabd746661c63d
mjeammet <marie.jeammet@gmail.com>
2021-10-07 22:35:27 +0200

    adding password change feature | generic views

--- LITReview/urls.py
+++ LITReview/urls.py
@@ -15,13 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 import authentication.views
 import reviews_webapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name="login"),
-    path('logout', authentication.views.logout_user, name="logout"),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True), 
+        name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('password_change', PasswordChangeView.as_view(
+        template_name = 'authentication/password_change.html'),
+        name="password_change"),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+        name='password_change_done'),
     path('home', reviews_webapp.views.home, name="home"),
 ]
