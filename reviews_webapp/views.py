7688fdeb2732c1f5d6155ff1a684dab7e07920e7
mjeammet <marie.jeammet@protonmail.com>
2021-10-11 14:43:02 +0200

    subscription page

--- reviews_webapp/views.py
+++ reviews_webapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.views.generic import View
 
 @login_required
 def home(request):
@@ -9,3 +8,21 @@ def home(request):
         "user": request.user,
     }
     return render(request, 'reviews_webapp/home.html', context)
+
+@login_required
+class SubscriptionPageView(View):
+    template_name = "reviews_webapp/subscriptions.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+    def post(self, request):
+        pass 
+
+def subscr(request):
+    template_name = "reviews_webapp/subscriptions.html"
+    context = {
+        "subscribed_to": ["Micheline"],
+        "subscribers": ["Micheline"],
+    }
+    return render(request, template_name, context)
